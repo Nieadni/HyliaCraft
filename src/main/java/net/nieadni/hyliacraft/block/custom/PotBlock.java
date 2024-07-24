9120a7692206338c005104f22a15bb29137c8b47
AngerVon2468 <136974526+AngerVon2468@users.noreply.github.com>
2024-07-24 18:41:50 +1000

    fixed I think, and I don't wanna touch those mixins I saw screaming in agony

--- src/main/java/net/nieadni/hyliacraft/block/custom/PotBlock.java
+++ src/main/java/net/nieadni/hyliacraft/block/custom/PotBlock.java
@@ -1,9 +1,12 @@
 package net.nieadni.hyliacraft.block.custom;
 
 import net.minecraft.block.*;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.*;
-import net.minecraft.util.function.BooleanBiFunction;
+import net.minecraft.world.BlockView;
+
+import java.util.stream.Stream;
 
 public class PotBlock extends Block {
 
@@ -12,15 +15,15 @@ public class PotBlock extends Block {
     }
 
     @Override
-    public VoxelShape getShape(BlockState state, BlockGetter blockGetter, @NotNull BlockPos blockPos, CollisionContext collisionContext) {
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return Stream.of(
-            Block.createCuboidShape(4, 0.6, 4, 12, 8.6, 12),
-            Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 9.1, 12.5),
-            Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 0.1, 12.5),
-            Block.createCuboidShape(3.5, 0.1, 12.5, 12.5, 9.1, 12.5),
-            Block.createCuboidShape(12.5, 0.1, 3.5, 12.5, 9.1, 12.5),
-            Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 9.1, 3.5),
-            Block.createCuboidShape(3.5, 0.1, 3.5, 3.5, 9.1, 12.5)
-            ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+                Block.createCuboidShape(4, 0.6, 4, 12, 8.6, 12),
+                Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 9.1, 12.5),
+                Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 0.1, 12.5),
+                Block.createCuboidShape(3.5, 0.1, 12.5, 12.5, 9.1, 12.5),
+                Block.createCuboidShape(12.5, 0.1, 3.5, 12.5, 9.1, 12.5),
+                Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 9.1, 3.5),
+                Block.createCuboidShape(3.5, 0.1, 3.5, 3.5, 9.1, 12.5)
+        ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
     }
 }
