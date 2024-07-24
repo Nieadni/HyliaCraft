9da6926a53a7d22fdf1f89d4526861761e184913
AngerVon2468 <136974526+AngerVon2468@users.noreply.github.com>
2024-07-24 18:29:18 +1000

    Update PotBlock.java
    Haven't tested if the imports work, I'm on github, not in intellij atm

--- src/main/java/net/nieadni/hyliacraft/block/custom/PotBlock.java
+++ src/main/java/net/nieadni/hyliacraft/block/custom/PotBlock.java
@@ -1,68 +1,26 @@
 package net.nieadni.hyliacraft.block.custom;
 
-import net.minecraft.block.Block;
-import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.shape.VoxelShape;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.util.shape.*;
+import net.minecraft.util.function.BooleanBiFunction;
 
 public class PotBlock extends Block {
 
-    //TODO: This is how it worked in Forge, it needs converting to Fabric
-
-    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-
-    public PotBlock(Settings properties) {
-        super(properties);
-    }
-
-    public VoxelShape makeShapeNS(){
-        VoxelShape shape = Shapes.empty();
-        shape = Shapes.join(shape, Shapes.create(0.25, 0.03125, 0.25, 0.75, 0.53125, 0.75), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.create(0.25, 0.03125, 0.25, 0.75, 0.53125, 0.75), BooleanOp.OR);
-
-        return shape;
-    }
-
-    public VoxelShape makeShapeEW(){
-        VoxelShape shape = Shapes.empty();
-        shape = Shapes.join(shape, Shapes.create(0.25, 0.03125, 0.25, 0.75, 0.53125, 0.75), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.create(0.25, 0.03125, 0.25, 0.75, 0.53125, 0.75), BooleanOp.OR);
-
-        return shape;
+    public PotBlock(Settings settings) {
+        super(settings);
     }
 
     @Override
-    public @NotNull VoxelShape getShape(BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos, @NotNull CollisionContext pContext) {
-        switch (pState.getValue(FACING)) {
-            case EAST, WEST -> {
-                return makeShapeEW();
-            }
-            case NORTH, SOUTH -> {
-                return makeShapeNS();
-            }
-        }
-        return makeShapeNS();
+    public VoxelShape getShape(BlockState state, BlockGetter blockGetter, @NotNull BlockPos blockPos, CollisionContext collisionContext) {
+        return Stream.of(
+            Block.createCuboidShape(4, 0.6, 4, 12, 8.6, 12),
+            Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 9.1, 12.5),
+            Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 0.1, 12.5),
+            Block.createCuboidShape(3.5, 0.1, 12.5, 12.5, 9.1, 12.5),
+            Block.createCuboidShape(12.5, 0.1, 3.5, 12.5, 9.1, 12.5),
+            Block.createCuboidShape(3.5, 0.1, 3.5, 12.5, 9.1, 3.5),
+            Block.createCuboidShape(3.5, 0.1, 3.5, 3.5, 9.1, 12.5)
+            ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
     }
-    /* FACING */
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
-    }
-
-    @Override
-    public BlockState rotate(BlockState pState, Rotation pRotation) {
-        return pState.setValue(FACING, pRotation.rotate(pState.getValue(FACING)));
-    }
-
-    @Override
-    public BlockState mirror(BlockState pState, Mirror pMirror) {
-        return pState.rotate(pMirror.getRotation(pState.getValue(FACING)));
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(FACING);
-    }
-
 }
