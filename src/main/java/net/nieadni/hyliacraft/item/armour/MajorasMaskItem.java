7e621df77051de1a89bf7a08c421c58f4bea5b2a
AngerVon2468 <thomaschad0909@gmail.com>
2024-07-26 08:18:41 +1000

    Updated Majora's Mask

--- src/main/java/net/nieadni/hyliacraft/item/armour/MajorasMaskItem.java
+++ src/main/java/net/nieadni/hyliacraft/item/armour/MajorasMaskItem.java
@@ -1,34 +1,32 @@
 package net.nieadni.hyliacraft.item.armour;
 
 import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
+
 import net.nieadni.hyliacraft.client.armour.MajorasMaskRenderer;
-import org.jetbrains.annotations.Nullable;
+
+import org.jetbrains.annotations.*;
+
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.animatable.client.GeoRenderProvider;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.animation.AnimatableManager;
-import software.bernie.geckolib.animation.AnimationController;
-import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.animation.*;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.logging.Level;
 
+// TODO: Make it so the mask makes hostile mobs neutral to player
+// TODO: Make it so the mask makes Endermen not attack when you look at them
 public class MajorasMaskItem extends ArmorItem implements GeoItem {
 
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
@@ -38,25 +36,26 @@ public class MajorasMaskItem extends ArmorItem implements GeoItem {
     }
 
     @Override
-    public void inventoryTick(ItemStack stack, World world, PlayerEntity player, int slot, boolean selected) {
-        super.inventoryTick(stack, world, player, slot, selected);
-        // Check if the world is not client-side
-        if (!world.isClient) {
+    public void inventoryTick(ItemStack stack, @NotNull World world, Entity entity, int slot, boolean selected) {
+        if (!world.isClient() && entity instanceof PlayerEntity player) {
+
             // Ensure the item is in the head slot
-            if (player.getEquippedStack(EquipmentSlot.HEAD).equals(stack)) {
+            if (player.getEquippedStack(EquipmentSlot.HEAD).isOf(this.asItem())) {
+
                 // Remove blindness and darkness effects
                 if (player.hasStatusEffect(StatusEffects.BLINDNESS)) {
+
                     player.removeStatusEffect(StatusEffects.BLINDNESS);
+
                 }
                 if (player.hasStatusEffect(StatusEffects.DARKNESS)) {
+
                     player.removeStatusEffect(StatusEffects.DARKNESS);
+
                 }
             }
         }
     }
-
-    //TODO: Make it so it makes hostiles mobs neutral to player
-    //TODO: Make it so it Endermen dont attack when you look at them
 
     public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
         tooltip.add(Text.translatable("tooltip.hyliacraft.majoras_mask1").formatted(Formatting.GRAY));
@@ -64,8 +63,7 @@ public class MajorasMaskItem extends ArmorItem implements GeoItem {
         tooltip.add(Text.translatable("tooltip.hyliacraft.majoras_mask3").formatted(Formatting.GRAY));
     }
 
-    // Geckolib Stuff
-
+    // Geckolib Stuff (:vomiting_face:)
     @Override
     public void createGeoRenderer(Consumer<GeoRenderProvider> consumer) {
         consumer.accept(new GeoRenderProvider() {
