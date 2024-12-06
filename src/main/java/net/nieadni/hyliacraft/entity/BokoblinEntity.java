63178b5409b8487420a8d0bf4b48d44dd06aa72e
Nieadni <diablomaster761@gmail.com>
2024-12-06 11:10:52 +0000

    Tried fixing but I need to go

--- src/main/java/net/nieadni/hyliacraft/entity/BokoblinEntity.java
+++ src/main/java/net/nieadni/hyliacraft/entity/BokoblinEntity.java
@@ -1,31 +1,39 @@
 package net.nieadni.hyliacraft.entity;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.InventoryOwner;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.Brain;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
 import net.nieadni.hyliacraft.entity.behaviours.TargetOrRetaliateAndHorn;
 import net.tslat.smartbrainlib.api.SmartBrainOwner;
 import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
 import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.Idle;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.TargetOrRetaliate;
 import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
 import net.tslat.smartbrainlib.util.BrainUtils;
 
 import java.util.List;
 
-public class BokoblinEntity extends PathAwareEntity implements SmartBrainOwner<BokoblinEntity>, InventoryOwner {
+public class BokoblinEntity extends MobEntity implements SmartBrainOwner<BokoblinEntity>, InventoryOwner {
     private boolean hasHorn;
     private int hornTimer = 0;
     private SimpleInventory inventory = new SimpleInventory(9);
 
-    protected BokoblinEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+    protected BokoblinEntity(EntityType<? extends MobEntity> entityType, World world) {
         super(entityType, world);
     }
 
