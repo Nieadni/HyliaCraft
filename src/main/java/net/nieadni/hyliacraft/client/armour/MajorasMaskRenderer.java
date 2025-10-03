3e7afc47691488e91ba2c7f83f9f209b2436746e
Nie <diablomaster761@gmail.com>
2025-10-03 10:02:03 +0100

    Emissives

--- src/main/java/net/nieadni/hyliacraft/client/armour/MajorasMaskRenderer.java
+++ src/main/java/net/nieadni/hyliacraft/client/armour/MajorasMaskRenderer.java
@@ -3,10 +3,12 @@ package net.nieadni.hyliacraft.client.armour;
 import net.nieadni.hyliacraft.item.armour.MajorasMaskItem;
 
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class MajorasMaskRenderer extends GeoArmorRenderer<MajorasMaskItem> {
 
     public MajorasMaskRenderer() {
         super(new MajorasMaskModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 }
