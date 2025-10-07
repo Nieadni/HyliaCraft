95702afb4421653cce9e8a39169a0776a8e2d34f
Nie <diablomaster761@gmail.com>
2025-10-07 10:43:45 +0100

    MOGMA MITT WORK

--- src/main/java/net/nieadni/hyliacraft/item/custom/MogmaMittItem.java
+++ src/main/java/net/nieadni/hyliacraft/item/custom/MogmaMittItem.java
@@ -1,28 +1,43 @@
 package net.nieadni.hyliacraft.item.custom;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CampfireBlock;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.component.type.ToolComponent;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.HoeItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.*;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import net.nieadni.hyliacraft.entity.HCEntities;
 import net.nieadni.hyliacraft.entity.sword_beam_entities.MasterSwordBeamEntity;
 import net.nieadni.hyliacraft.item.materials.MogmaMittMaterial;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import static net.minecraft.item.HoeItem.TILLING_ACTIONS;
+import static net.minecraft.item.ShovelItem.PATH_STATES;
 
 public class MogmaMittItem extends Item {
     public static final Identifier TOOL_RANGE_MODIFIER_ID = Identifier.of("hyliacraft", "tool_entity_reach");
@@ -52,18 +67,69 @@ public class MogmaMittItem extends Item {
 
     //TODO: Use on block = path, crouch + use on block = till
 
-    /*
     @Override
-    public ActionResult useOnBlock(idk) {
-
-        ItemStack stack = user.getMainHandStack();
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        BlockPos blockPos = context.getBlockPos();
+        BlockState blockState = world.getBlockState(blockPos);
+        PlayerEntity user = context.getPlayer();
+        Pair<Predicate<ItemUsageContext>, Consumer<ItemUsageContext>> pair = TILLING_ACTIONS.get(world.getBlockState(blockPos).getBlock());
+        assert user != null;
         if (user.isSneaking()) {
-            // till
+            if (pair == null) {
+                return ActionResult.PASS;
+            } else {
+                Predicate<ItemUsageContext> predicate = pair.getFirst();
+                Consumer<ItemUsageContext> consumer = pair.getSecond();
+                if (predicate.test(context)) {
+                    PlayerEntity playerEntity = context.getPlayer();
+                    world.playSound(playerEntity, blockPos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    if (!world.isClient) {
+                        consumer.accept(context);
+                        if (playerEntity != null) {
+                            context.getStack().damage(1, playerEntity, LivingEntity.getSlotForHand(context.getHand()));
+                        }
+                    }
+
+                    return ActionResult.success(world.isClient);
+                } else {
+                    return ActionResult.PASS;
+                }
+            }
         } else {
-            // path
+            if (context.getSide() == Direction.DOWN) {
+                return ActionResult.PASS;
+            } else {
+                PlayerEntity playerEntity = context.getPlayer();
+                BlockState blockState2 = PATH_STATES.get(blockState.getBlock());
+                BlockState blockState3 = null;
+                if (blockState2 != null && world.getBlockState(blockPos.up()).isAir()) {
+                    world.playSound(playerEntity, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    blockState3 = blockState2;
+                } else if (blockState.getBlock() instanceof CampfireBlock && blockState.get(CampfireBlock.LIT)) {
+                    if (!world.isClient()) {
+                        world.syncWorldEvent(null, 1009, blockPos, 0);
+                    }
+
+                    CampfireBlock.extinguish(context.getPlayer(), world, blockPos, blockState);
+                    blockState3 = blockState.with(CampfireBlock.LIT, false);
+                }
+
+                if (blockState3 != null) {
+                    if (!world.isClient) {
+                        world.setBlockState(blockPos, blockState3, 11);
+                        world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, blockState3));
+                        if (playerEntity != null) {
+                            context.getStack().damage(1, playerEntity, LivingEntity.getSlotForHand(context.getHand()));
+                        }
+                    }
+
+                    return ActionResult.success(world.isClient);
+                } else {
+                    return ActionResult.PASS;
+                }
+            }
         }
-        return ActionResult.PASS;
     }
-     */
 
 }
